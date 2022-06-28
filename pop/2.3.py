6d0a6d4627e35d22f33412f0c029bea9331ae760
Yago César <yagocdj@gmail.com>
2022-06-28 11:08:17 -0300

    Questão 2.3 resolvida

--- pop/2.3.py
+++ pop/2.3.py
@@ -2,6 +2,13 @@
 
 n = int(input())  # representa o número de nomes a serem lidos
 names = [input().upper() for i in range(n)]
+names_norepeat = []
 
-for name in names:
-    name_counter = names.count(name)
+# adicionando os nomes a uma nova lista sem nomes repetidos
+for i in range(n):
+    if names[i] not in names_norepeat:
+        names_norepeat.append(names[i])
+
+# exibindo a nova lista sem nomes repetidos
+for name in names_norepeat:
+    print(name)
