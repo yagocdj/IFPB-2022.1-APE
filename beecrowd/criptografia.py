9ecb41e7abd9c8ef70928f75f8127e7899d33128
Yago César <yagocdj@gmail.com>
2022-06-29 01:35:49 -0300

    resolvendo a questão 'Criptografia' do Beecrowd

---
+++ beecrowd/criptografia.py
@@ -0,0 +1,27 @@
+# Questão "Criptografia"
+
+N = int(input())  # quantidade de strings a serem criptografadas
+# Lista contendo as strings a serem criptografadas
+words_list = [input() for i in range(N)]
+encrypted_words_list = []
+
+# Passada 1 - deslocar os caracteres
+transform_unicode = int()
+transform_character = str()
+shift_position = int()
+
+for word in words_list:
+    encrypted_word = str()
+
+    for character in word:
+        if (character >= 'a' and character <= 'z') or \
+                (character >= 'A' and character <= 'Z'):
+            transform_unicode = ord(character)
+            shift_position = transform_unicode + 3
+            transform_character = chr(shift_position)
+            encrypted_word += transform_character
+    encrypted_words_list.append(encrypted_word)
+
+print(encrypted_words_list)
+
+# Passada 2
