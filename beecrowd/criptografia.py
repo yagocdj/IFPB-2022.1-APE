20d01670ba413c91719433d1c7f97276616ba8a1
Yago César <yagocdj@gmail.com>
2022-07-12 13:51:08 -0300

    Finalizando a lógica da questão 'Criptografia' do Beecrowd

--- beecrowd/criptografia.py
+++ beecrowd/criptografia.py
@@ -3,12 +3,10 @@
 N = int(input())  # quantidade de strings a serem criptografadas
 
 # Lista contendo as strings a serem criptografadas
-
 words_list = [input() for i in range(N)]
-encrypted_words_list = []
+partial_encrypted_list = []
 
 # Passada 1 - deslocar os caracteres que sejam letras minúsculas e maiúsculas
-
 transform_to_unicode = int()
 transform_to_character = str()
 shift_position = int()
@@ -21,27 +19,48 @@ for word in words_list:
     # Percorrendo cada carácter da palavra
     for character in word:
         if character in letters:
+
             # Transformando o carácter em código unicode
             transform_to_unicode = ord(character)
+
             # Deslocando o carácter 3 posições para direita
             shift_position = transform_to_unicode + 3
+
             # Transformando o código unicode no seu respectivo carácter
             transform_to_character = chr(shift_position)
+
             # Adicionando o carácter à nova string criptografada
             encrypted_word += transform_to_character
+
         # Caso o carácter não seja uma letra maiúscula ou minúscula
         else:
             encrypted_word += character
     # Adicionando a string criptografada a uma nova lista
-    encrypted_words_list.append(encrypted_word)
+    partial_encrypted_list.append(encrypted_word)
 
 # Passada 2 - inverter as palavras
-for i in range(len(encrypted_words_list)):
-    encrypted_words_list[i] = encrypted_words_list[i][::-1]
+for i in range(len(partial_encrypted_list)):
+    partial_encrypted_list[i] = partial_encrypted_list[i][::-1]
 
 # Passada 3 - caracteres da metade para frente serão deslocados uma posição
 # para a esquerda
 # de acordo com a tabela ASCII
 
 # Percorrendo a lista de palavras criptografadas
-for word in encrypted_words_list:
+
+# Lista que terá as palavras completamente criptografadas
+final_encrypted_list = []
+
+for word in partial_encrypted_list:
+    encrypted_word = str()
+    for index in range(len(word)):
+        if index >= len(word) // 2:
+            transform_to_unicode = ord(word[index])
+            shift_position = transform_to_unicode - 1
+            transform_to_character = chr(shift_position)
+            encrypted_word += transform_to_character
+        else:
+            encrypted_word += word[index]
+    final_encrypted_list.append(encrypted_word)
+
+print(final_encrypted_list)
