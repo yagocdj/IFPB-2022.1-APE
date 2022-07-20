0e38e9294dba071cdc42f28da971f125b1e9ccf5
Yago César <yagocdj@gmail.com>
2022-07-20 17:52:19 -0300

    Questão 2.4 do G4M3 resolvida

--- pop/2.4.py
+++ pop/2.4.py
@@ -1,7 +1,23 @@
 # Questão 2.4
 
-quantidade_versos = 4
-# lendo os versos e armazenando-os em uma lista
-versos = [input().lower() for i in range(quantidade_versos)]
+lines_number = 4
+# armazenar os 4 versos do poema
+poems_list = [input() for index in range(lines_number)]
 
-# TERMINAR ESTE PROGRAMA
+# soma que representa a beleza do poema
+counter = 0
+
+# comparar os versos dois a dois
+for i in range(lines_number // 2):
+
+    verse_last_word = poems_list[i].split()[-1]
+    rime_last_word = poems_list[i+2].split()[-1]
+
+    verse_last_letters = verse_last_word[-3:]
+    rime_last_letters = rime_last_word[-3:]
+
+    for j in range(len(verse_last_letters)):
+        if verse_last_letters[j] == rime_last_letters[j]:
+            counter += 1
+
+print(counter)
