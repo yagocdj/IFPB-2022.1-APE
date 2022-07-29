68a1dc70ed6d492e71f32424a6b44ab9c22f1cd9
Yago César <yagocdj@gmail.com>
2022-07-29 17:27:14 -0300

    Progredindo na questão 3.2 do G4M3

--- pop/3.2.py
+++ pop/3.2.py
@@ -1,5 +1,8 @@
 # Questão 3.2
 
-# quantidade de casas do tabuleiro
-n = int(input())  # ímpar
+# quantidade de casas do tabuleiro (sempre ímpar)
+n = int(input())
 coins_number = list(map(int, (input().split())))
+middle_place = len(coins_number) // 2
+
+# TERMINAR ESTE PROGRAMA
